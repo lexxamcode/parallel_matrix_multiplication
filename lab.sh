17722b30abc05bcfa5da9c2240ef5c5734ce6c1a
lexxamcode <lexamishkin@mail.ru>
2022-12-11 15:49:01 +0300

    Little automatization

--- lab.sh
+++ lab.sh
@@ -1,5 +1,5 @@
 gcc -std=gnu++11 -fopenmp ./create.cpp -o create.o
-mpic++ load.cpp -o load.o
+mpicxx load.cpp -o load.o
 for ((size=50; size <=1600; size = size*2)); do
 ./create.o $size
 mpiexec -n 2 ./load.o $size
